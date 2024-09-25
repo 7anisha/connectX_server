a190a76869ff938aa7b15170ba82f028aee55ea2
Anisha Rani <2021pietcsanisha021@poornima.org>
2024-09-25 23:28:44 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -13,10 +13,14 @@ const adminRoute = require("./routes/admin-router");
 require("dotenv").config;
 
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://example.vercel.app", "https://another-domain.com","https://connect-x-client.vercel.app/"],
+  origin: ["http://localhost:5173","https://connect-x-client.vercel.app"],
   methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
   credentials: true,
 };
+
+// app.use(cors({
+//   origin: 'https://connect-x-client.vercel.app',
+// }));
 
 
 app.use(cors(corsOptions));
